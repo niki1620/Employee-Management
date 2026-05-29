b82d88af0b0911656a257890a578e85d10a0e80d
niki1620 <nikitha1620@gmail.com>
2026-05-29 13:43:47 +0530

    api routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,9 +6,14 @@ const employeeRoutes = require("./routes/employeeRoutes");
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
+// Root route (FIX)
+app.get("/", (req, res) => {
+  res.send("Employee Management API is running 🚀");
+});
+
+// API routes
 app.use("/api/employees", employeeRoutes);
 
 module.exports = app;
