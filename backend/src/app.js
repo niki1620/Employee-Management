3c22c74f81a5177816391af764a3130e265500dc
niki1620 <nikitha1620@gmail.com>
2026-05-29 18:34:29 +0530

    vercel domain link

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://employee-management-jet-nu.vercel.app/"
+    "https://employee-management-jet-nu.vercel.app"
   ],
   credentials: true
 }));
